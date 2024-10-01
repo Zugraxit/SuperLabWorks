83ac94ecda54f3f455c10abba527c11f30409588
Zugraxit <154356446+Zugraxit@users.noreply.github.com>
2024-10-01 15:46:55 +0300

    dfbhdhdf

---
+++ WebApplication1/WebApplication1/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok(new { status = "helth dibidi" });
+        }
+    }
+}
